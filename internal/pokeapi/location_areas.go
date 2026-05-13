e4a39fe137cbc11fcd175b88cc64972a891aa431
Mario Coloma <marioxcolomar@gmail.com>
2026-05-13 12:46:58 +0200

    Move types to location.

--- internal/pokeapi/location_areas.go
+++ internal/pokeapi/location_areas.go
@@ -3,43 +3,40 @@ package pokeapi
 import (
 	"encoding/json"
 	"io"
-	"log"
 	"net/http"
-	"pokedex/internal/pokecache"
 )
 
-func (c *Client) ListLocationAreas(pageUrl *string) (pokecache.LocationAreaResponse, error) {
-	url := baseUrl + "/location-area"
+func (c *Client) ListLocationAreas(pageUrl *string) (LocationAreaResponse, error) {
+	url := baseUrl + "/location-area/"
 	if pageUrl != nil {
 		url = *pageUrl
 	}
 	if val, ok := c.cache.Get(url); ok {
-		cacheJson := pokecache.LocationAreaResponse{}
+		cacheJson := LocationAreaResponse{}
 		err := json.Unmarshal(val, &cacheJson)
 		if err != nil {
-			return pokecache.LocationAreaResponse{}, err
+			return LocationAreaResponse{}, err
 		}
 		return cacheJson, nil
 	}
 
-	res, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return pokecache.LocationAreaResponse{}, err
+		return LocationAreaResponse{}, err
 	}
-	body, err := io.ReadAll(res.Body)
-	defer res.Body.Close()
-	if res.Response.StatusCode > 299 {
-		log.Fatalf("Response failed with status code: %d and \nbody: %s\n", res.Response.StatusCode, body)
-		return pokecache.LocationAreaResponse{}, err
-	}
+	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		log.Fatal(err)
-		return pokecache.LocationAreaResponse{}, err
+		return LocationAreaResponse{}, err
 	}
-	locationAreas := pokecache.LocationAreaResponse{}
+	body, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return LocationAreaResponse{}, err
+	}
+	locationAreas := LocationAreaResponse{}
 	errJson := json.Unmarshal(body, &locationAreas)
 	if errJson != nil {
-		return pokecache.LocationAreaResponse{}, nil
+		return LocationAreaResponse{}, nil
 	}
 	c.cache.Add(url, body)
 	return locationAreas, nil
--- internal/pokeapi/location_types.go
+++ internal/pokeapi/location_types.go
@@ -1,4 +1,4 @@
-package pokecache
+package pokeapi
 
 type LocationAreaResponse struct {
 	Count    int     `json:"count"`
@@ -11,33 +11,9 @@ type LocationAreaResponse struct {
 }
 
 type ExploreResponse struct {
-	ID                   int    `json:"id"`
-	Name                 string `json:"name"`
-	GameIndex            int    `json:"game_index"`
-	EncounterMethodRates []struct {
-		EncounterMethod struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"encounter_method"`
-		VersionDetails []struct {
-			Rate    int `json:"rate"`
-			Version struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version"`
-		} `json:"version_details"`
-	} `json:"encounter_method_rates"`
-	Location struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"location"`
-	Names []struct {
-		Name     string `json:"name"`
-		Language struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"language"`
-	} `json:"names"`
+	ID                int    `json:"id"`
+	Name              string `json:"name"`
+	GameIndex         int    `json:"game_index"`
 	PokemonEncounters []struct {
 		Pokemon struct {
 			Name string `json:"name"`
