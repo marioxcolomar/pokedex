717820174d9ac8a08512ceb4a45e2a837e823a16
Mario Coloma <marioxcolomar@gmail.com>
2026-05-13 12:47:43 +0200

    Add explore command.

---
+++ command_explore.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func commandExplore(config *commandConfig) error {
+	res, err := config.pokeapiClient.ListPokemonPerLocationArea(*config.id)
+	if err != nil {
+		return err
+	}
+	for _, encounter := range res.PokemonEncounters {
+		fmt.Println(encounter.Pokemon.Name)
+	}
+	return nil
+}
