c75d8deb9ef8beca69f119e262970d8c81843079
Mario Coloma <marioxcolomar@gmail.com>
2026-05-14 15:30:27 +0200

    Rename types file.

--- internal/pokeapi/types.go
+++ internal/pokeapi/types.go
@@ -38,3 +38,13 @@ type ExploreResponse struct {
 		} `json:"version_details"`
 	} `json:"pokemon_encounters"`
 }
+
+type Pokemon struct {
+	ID             int    `json:"id"`
+	Name           string `json:"name"`
+	BaseExperience int    `json:"base_experience"`
+	Height         int    `json:"height"`
+	IsDefault      bool   `json:"is_default"`
+	Order          int    `json:"order"`
+	Weight         int    `json:"weight"`
+}
